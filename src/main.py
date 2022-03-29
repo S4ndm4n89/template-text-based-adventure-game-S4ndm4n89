894dd25c95ea1195d943cfd59aaa6c138393865f
Bwilliams17 <29454@bridgend.ac.uk>
2022-03-29 09:35:59 +0100

    fixs for key number

--- src/main.py
+++ src/main.py
@@ -15,23 +15,23 @@ def main():
     print("Upon looking around this central room again you find a backpack and put it on,\n you also notice door like shape in the corner of the room, it has four key like hole init.   ")  
 
     while True:
-        Colour = input("Which room do you want to try? please choice from Red, Blue, Green or Black. ")    
+        Colour = input("Which room do you want to try? please choice from Red, Blue, Green or Black. ").lower().strip()    
 
-        if Colour == "Red":
+        if Colour == "red":
                 room_colour = "Red"
                 puzzle = " what is 2 + 2?"
                 puzzle_solution = "4"
                 key_number = "1"
                 lives = in_room(backpack,lives,room_colour,puzzle,puzzle_solution,key_number)
                
-        elif Colour == "Blue":
+        elif Colour == "blue":
                 room_colour = "Blue"
                 puzzle = " what is the value of this binary 10100 ?"
                 puzzle_solution = "20"
                 key_number = "2"
                 lives = in_room(backpack,lives,room_colour,puzzle,puzzle_solution,key_number)
 
-        elif Colour == "Green":
+        elif Colour == "green":
                 room_colour = "Green"
                 puzzle = " what is 2*8  ?"
                 puzzle_solution = "16"
@@ -39,7 +39,7 @@ def main():
                 lives = in_room(backpack,lives,room_colour,puzzle,puzzle_solution,key_number)
 
         
-        elif Colour == "Black": 
+        elif Colour == "black": 
                 room_colour = "Black"
                 puzzle = " what is the value of this binary 1100100 ?"
                 puzzle_solution = "100"
@@ -62,7 +62,7 @@ def in_room(backpack,lives,room_colour,puzzle,puzzle_solution,key_number):
         print(f"you have entered the {room_colour} room. ")
         puzzle_guess = input(puzzle)
         if puzzle_guess == puzzle_solution:
-                print("Correct, {Key_number} collected.") 
+                print(f"Correct, {key_number} collected.") 
                #checking if key is allready in back pack
                 if f"key {key_number}" not in backpack:
                         backpack.append(f"key {key_number}")
@@ -74,15 +74,15 @@ def in_room(backpack,lives,room_colour,puzzle,puzzle_solution,key_number):
 
         return lives  
 
-def addition(a,b):
+#def addition(a,b):
         return a+b+5
 #test need test_......
-def test_addition():
+#def test_addition():
         assert addition(3,5) == 8
         assert addition(-1,0) ==-1
         assert addition(-1,1) == 0
 
-def test_in_room():#test what happen when answer is wrong.
+#def test_in_room():#test what happen when answer is wrong.
 
         backpack =[]
         lives = 3
@@ -106,4 +106,4 @@ if __name__ == "__main__":
         #value = addition(3,2)
         #print(value)
         #test_addition()
-        test_in_room()
+        #test_in_room()
